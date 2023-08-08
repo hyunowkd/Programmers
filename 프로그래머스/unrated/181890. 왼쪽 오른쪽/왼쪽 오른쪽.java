19422264859c2be7ca8856959a9b4f54e22928d0
hyunowkd <65902105+hyunowkd@users.noreply.github.com>
2023-08-08 17:45:37 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -4,18 +4,16 @@ class Solution {
     public String[] solution(String[] str_list) {
         int index = 0;
         int dir = 0;
-        
-        for(int i=0; i<str_list.length; i++){
-            if(str_list[i].equals("l")){index = i; dir = -1; break;}
-            else if(str_list[i].equals("r")){index = i; dir = 1; break;}
-        }
-        
         String[] answer = {};
         
-        if(dir < 0){
-            answer = Arrays.copyOfRange(str_list, 0, index);
-        }else{
-            answer = Arrays.copyOfRange(str_list, index+1, str_list.length);
+        for(int i=0; i<str_list.length; i++){
+            if(str_list[i].equals("l")){
+                answer = Arrays.copyOfRange(str_list, 0, i);
+                break;
+            }else if(str_list[i].equals("r")){
+                answer = Arrays.copyOfRange(str_list, i+1, str_list.length);
+                break;
+            }
         }
         
         return answer;
