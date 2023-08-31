a728592d59714d8dd3c12835f32613044f922c54
조현호 <65902105+hyunowkd@users.noreply.github.com>
2023-08-31 13:26:08 +0900

    [unrated] Title: 세 개의 구분자, Time: 55.46 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myStr) {
-        List<String> list = Arrays.asList(myStr.replace("a", " ").replace("b", " ").replace("c", " ").split(" "));
-        
-        String[] arr = list.stream().filter(i->i.length()!=0).toArray(String[]::new);
+        String[] arr = Arrays.asList(myStr.split("a|b|c")).stream().filter(i->!i.isEmpty()).toArray(String[]::new);
         String[] empty = {"EMPTY"};
+        
         return arr.length == 0? empty : arr;
     }
 }
